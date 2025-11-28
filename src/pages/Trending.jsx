e73c18a0fb52a97d838be378a463bb9ff18e6b27
Shubham3698 <pandey0shubham3698@gmail.com>
2025-11-28 19:31:12 +0530

    Update Trending.jsx

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -3,8 +3,6 @@ import Nwmasonry from "../components/Nwmasonry";
 
 export default function Sticker() {
   const stickerImages = [
-    "https://i.pinimg.com/1200x/da/d0/ae/dad0ae19bc31931008317564ba5ff832.jpg", 
-    "https://i.pinimg.com/1200x/da/d0/ae/dad0ae19bc31931008317564ba5ff832.jpg", 
     "https://i.pinimg.com/1200x/5a/ad/0a/5aad0a8534ff652ffab1788930547741.jpg",
     "https://i.pinimg.com/736x/48/ba/91/48ba918c7fbdeee6056a7e664f35481b.jpg",
     "https://i.pinimg.com/1200x/8b/0c/14/8b0c14adee9fc943bc139a125a0fc400.jpg",
