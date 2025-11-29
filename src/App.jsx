e255bbb1e5b90fc14cd5f9da3b2bfdb337aa98ce
shubham3698 <pandey0shubham3698@gmail.com>
2025-11-29 18:44:28 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HorizontalTopMenu from "./components/tpmenu";
-import About from "./pages/about";
+import MainNavbar from "./components/Maninav";
+import Trending from "./pages/Trending";
+import About from "./pages/About";
 import Sticker from "./pages/Sticker";
-import Poster from "./pages/Poster"
-import Trending from "./pages/Trending"
-import Goodies from "./pages/Goodies"
-import Testnav from "./components/Testnav"
-import Logonav from "./components/Logonav"
-import MNv from "./components/Maninav"
-
-// import { div } from "framer-motion/client";
+import Poster from "./pages/Poster";
+import Goodies from "./pages/Goodies";
 
 function App() {
   return (
-    <div style={{background:"#fff3eb"}} >
+    <div style={{ background: "#fff3eb" }}>
       <BrowserRouter>
-        {/* <HorizontalTopMenu /> */}
-        {/* <Testnav/> */}
-        {/* <Logonav/> */}
-        <MNv/>
-        <div style={{ marginTop: "105px" }}></div>
+        <MainNavbar />
 
         <Routes>
           <Route path="/" element={<Trending />} />
           <Route path="/about" element={<About />} />
-          <Route path="/sticker" element={<Sticker/>} />
-          <Route path="/poster" element={<Poster/>} />
-          <Route path="/goodies" element={<Goodies/>} />
+          <Route path="/sticker" element={<Sticker />} />
+          <Route path="/poster" element={<Poster />} />
+          <Route path="/goodies" element={<Goodies />} />
         </Routes>
       </BrowserRouter>
     </div>
